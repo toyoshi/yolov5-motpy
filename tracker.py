82bba0f640c31ea9a0a4885fe94c1a432b8eb4c9
toyoshi <r@toyoshi.jp>
2021-03-18 14:13:58 +0900

    move yolov5

--- tracker.py
+++ tracker.py
@@ -111,20 +111,12 @@ def detect(save_img=False):
                     object_box = np.array([int(xyxy[0]), int(xyxy[1]), int(xyxy[2]), int(xyxy[3])])
                     out_detections.append(Detection(box=object_box, score=conf.to('cpu')))
 
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
-
-                    if save_img or view_img:  # Add bbox to image
-                        label = f'{names[int(cls)]} {conf:.2f}'
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-
-                print(out_detections)
                 tracker.step(out_detections)
                 tracks = tracker.active_tracks(3)
-                print(tracks)
+
+                for track in tracks:
+                    label = f'{track.id[:5]}'
+                    plot_one_box(track.box, im0, label=label, color=colors[int(cls    )], line_thickness=3)
 
 
             # Print time (inference + NMS)
